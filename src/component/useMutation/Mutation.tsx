bf21cdb7de39330fa7902b37019366b37359f72c
kangactor123 <kangactor123@naver.com>
2023-03-05 09:31:55 +0900

    useMutate update/ folder structure change

--- src/component/useMutation/Mutation.tsx
+++ src/component/useMutation/Mutation.tsx
@@ -39,9 +39,17 @@ function Mutation() {
       onSettled: () => setStatus(Status.Loading),
       onSuccess: () => {
         setStatus(Status.Success);
+        // invalidateQueries : QueryClient 에서 제공하는 메서드로, 주어지는 키에 매칭되는 쿼리를 stale 한 상태로 만들어서 리패칭 하게 하는 메서드
+        // 기본적으로 키를 제공했을 경우 배열속에 같은 키가 들어있다면 매칭되는 모든 쿼리들이 stale 하게 상태가 변화됨
+        // { exact: true } 옵션을 넣어주면 정확히 매칭이 가능.
         client.invalidateQueries(Query_KEY);
       },
-      onError: () => setStatus(Status.Error),
+      onError: (err) => {
+        setStatus(Status.Error);
+        // setQueryData: mutation 성공 후 해당 서버 데이터를 패칭하지 않고도 데이터를 바꿔줄 수 있음
+        // cached 된 쿼리의 데이터를 즉시 변경해줌
+        client.setQueryData([Query_KEY, Status.Error], { err });
+      },
     }
   );
 
